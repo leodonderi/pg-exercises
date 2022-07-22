8fda2ced10baabe7f4e0bc4c446cdfde6b6dda1b
leodonderi <leonardo.rodrigues@mercadolivre.com>
2022-07-22 18:25:06 -0300

    Exercicios express 22.07

---
+++ express/produtos.js
@@ -0,0 +1,10 @@
+const products = [
+    { id: 1, name: 'Macbook', price: 1300, quantity: 40, colors: ['silver', 'black', 'white'] },
+    { id: 2, name: 'Iphone', price: 1000, quantity: 50, colors: ['silver', 'red', 'white'] },
+    { id: 3, name: 'Pendrive', price: 10, quantity: 10, colors: [] },
+    { id: 4, name: 'Headset', price: 50, quantity: 0, colors: ['black'] },
+    { id: 5, name: 'Mouse', price: 20, quantity: 5, colors: ['white', 'black', 'blue'] },
+    { id: 6, name: 'Tablet', price: 500, quantity: 20, colors: ['white', 'black'] },
+]
+
+module.exports = products;
